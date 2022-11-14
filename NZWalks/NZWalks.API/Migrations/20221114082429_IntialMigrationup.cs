fe5aa857e42992506c6312e5d605f6b2546ef855
QuachThao2010 <quachthao2010@gmail.com>
2022-11-14 15:55:00 +0700

    End Of Section 2

---
+++ NZWalks/NZWalks.API/Migrations/20221114082429_IntialMigrationup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    public partial class IntialMigrationup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "popular",
+                table: "Regions",
+                newName: "population");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "population",
+                table: "Regions",
+                newName: "popular");
+        }
+    }
+}
