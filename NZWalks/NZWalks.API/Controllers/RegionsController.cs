46bbf8f4297d91e461d5ef3f629f5a9eafb00dad
QuachThao2010 <quachthao2010@gmail.com>
2022-11-15 11:14:06 +0700

    End of Section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegoins()
+        {
+            var regions = await regionRepository.GetAllAsync();
+
+            var regionsDTO = mapper.Map<List<Models.DTO.Regoin>>(regions);
+            return Ok(regionsDTO );
+        }
+    }
+}
